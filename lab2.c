7b0e445f08c7801becc9252fa457a31a0f99dde2
Jawad <jawadchy15@gmail.com>
2025-05-12 22:18:15 -0400

    lab2 init

---
+++ lab2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void lonely()
+{
+    puts("nobody ever calls me...\n");
+}
+
+int main()
+{
+    char buf[8];
+    scanf("%s",buf);
+    printf("you entered %s\n",buf);
+    return 0;
+}
+
+// jumping to code written in stack
+
+//
---
+++ shell.s
@@ -0,0 +1,6 @@
+pop %rdi
+xor %rax, %rax
+movb $0x3b, %al
+xor %rsi, %rsi
+xor %rdx, %rdx
+syscall
